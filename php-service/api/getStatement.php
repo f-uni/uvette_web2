1d9e46fcd8abe6f3c1443d64d0c442a781401ce8
Filippo Colpani <f.colpani3@studenti.unibg.it>
2024-07-10 22:56:31 +0200

    Update getStatement.php

--- php-service/api/getStatement.php
+++ php-service/api/getStatement.php
@@ -14,12 +14,56 @@ function getCreateStatement($conn,$table_name){
     return null;
 }
 
+function convertMysqlToPostgres($mysqlCreateStatement) {
+    // Mappa dei tipi di dati MySQL a PostgreSQL
+    $dataTypeMapping = [
+        '/\bint\b/' => 'integer',
+        '/\bsmallint\b/' => 'smallint',
+        '/\btinyint\b/' => 'smallint', // Non c'è un tinyint in PostgreSQL, quindi si può mappare a smallint
+        '/\bmediumint\b/' => 'integer',
+        '/\bbigint\b/' => 'bigint',
+        '/\bvarchar\((\d+)\)\b/' => 'varchar($1)',
+        '/\bchar\((\d+)\)\b/' => 'char($1)',
+        '/\btext\b/' => 'text',
+        '/\bblob\b/' => 'bytea',
+        '/\bfloat\b/' => 'real',
+        '/\bdouble\b/' => 'double precision',
+        '/\bdecimal\((\d+),(\d+)\)\b/' => 'numeric($1,$2)',
+        '/\btimestamp\b/' => 'timestamp',
+        '/\bdatetime\b/' => 'timestamp',
+        '/\bdate\b/' => 'date',
+        '/\btime\b/' => 'time',
+        '/\byear\b/' => 'integer'
+    ];
+
+    // Rimuove ENGINE, CHARSET e COLLATE
+    $mysqlCreateStatement = preg_replace(
+        ['/ENGINE=\w+/', '/DEFAULT CHARSET=\w+/', '/COLLATE=\w+/'],
+        '',
+        $mysqlCreateStatement
+    );
+
+    // Rimuove eventuali spazi in più
+    $mysqlCreateStatement = preg_replace('/\s+/', ' ', $mysqlCreateStatement);
+
+    // Sostituzione dei tipi di dati
+    foreach ($dataTypeMapping as $mysqlType => $postgresType) {
+        $mysqlCreateStatement = preg_replace($mysqlType, $postgresType, $mysqlCreateStatement);
+    }
+
+    // Rimuove AUTO_INCREMENT (in PostgreSQL si usa SERIAL per colonne con auto-incremento)
+    $mysqlCreateStatement = preg_replace('/\bAUTO_INCREMENT\b/', '', $mysqlCreateStatement);
+
+    // Restituisce lo statement convertito
+    return trim($mysqlCreateStatement);
+}
+
 $table_param=$_GET["table"];
 
 $statement=getCreateStatement($conn, $table_param);
 
 if($statement){
-    json_response(200, ["statement"=>$statement]);
+    json_response(200, ["statement"=>convertMysqlToPostgres($statement),"statementmy"=>$statement]);
 }else{
     json_response(400, ["error"=>"table doesn't exist"]);
 }
